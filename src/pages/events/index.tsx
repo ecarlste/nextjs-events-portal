4f843f50b1c542bb3483399b65552dcb24080ee1
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-01 13:56:04 -0600

    Updated all events page name

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,7 +1,7 @@
-export default function EventPage() {
+export default function AllEventsPage() {
   return (
     <div>
-      <h1>This is the events page</h1>
+      <h1>This is the all events page</h1>
     </div>
   );
 }
