1dd0bc578a0f17d72f0a6e88a4c0442567e8906b
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-02 16:20:54 -0600

    Added routing for event search form

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,14 +1,22 @@
 import EventList from '@/components/events/event-list';
 import EventsSearch from '@/components/events/events-search';
 import { getAllEvents } from '@/fake-data';
+import { useRouter } from 'next/router';
 import { Fragment } from 'react';
 
 export default function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year: string, month: string) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} />
     </Fragment>
   );
