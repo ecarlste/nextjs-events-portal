cb56e58509e18f93518847d0b83b73500eb92660
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-01 19:25:04 -0600

    Added event list and item with css styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import EventList from '@/components/events/event-list';
+import { getFeaturedEvents } from '@/fake-data';
+
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>This is the events home page</h1>
+      <EventList events={featuredEvents} />
     </div>
   );
 }
