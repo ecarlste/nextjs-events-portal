fbaa9159a4ee859e05d6b3890f08151eeaa99a53
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-01 22:34:46 -0600

    Added events search component and styling

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,12 +1,15 @@
 import EventList from '@/components/events/event-list';
+import EventsSearch from '@/components/events/events-search';
 import { getAllEvents } from '@/fake-data';
+import { Fragment } from 'react';
 
 export default function AllEventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList events={events} />
-    </div>
+    </Fragment>
   );
 }
