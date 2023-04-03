89dd8c4745cbfab0c8987c5c4d7ef8481bb766e4
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-02 19:45:35 -0600

    Added some more formatting to several event pages

--- src/pages/events/[...slug].tsx
+++ src/pages/events/[...slug].tsx
@@ -1,7 +1,44 @@
+import EventList from '@/components/events/event-list';
+import ResultsTitle from '@/components/events/results-title';
+import Button from '@/components/ui/button';
+import ErrorAlert from '@/components/ui/error-alert';
+import { getFilteredEvents } from '@/fake-data';
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+
 export default function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filteredData = router.query.slug;
+
+  console.log(filteredData);
+
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const numYear = +filteredData[0];
+  const numMonth = +filteredData[1];
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the specified filter...</p>
+        </ErrorAlert>
+        <Button link="/">Show All Events</Button>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
-      <h1>Filtered events page</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList events={filteredEvents} />
+    </Fragment>
   );
 }
--- src/pages/events/[eventId].tsx
+++ src/pages/events/[eventId].tsx
@@ -1,6 +1,7 @@
 import EventContent from '@/components/event-detail/event-content';
 import EventLogistics from '@/components/event-detail/event-logistics';
 import EventSummary from '@/components/event-detail/event-summary';
+import ErrorAlert from '@/components/ui/error-alert';
 import { getEventById } from '@/fake-data';
 import { useRouter } from 'next/router';
 import { Fragment } from 'react';
@@ -12,7 +13,11 @@ export default function EventDetailPage() {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
   }
 
   return (
