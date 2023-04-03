cdd8bc0c7e391c5989d8458c344bc52d54335d72
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-03 10:58:24 -0600

    Home page uses static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import EventList from '@/components/events/event-list';
-import { getFeaturedEvents } from '@/fake-data';
+import { getFeaturedEvents } from '@/helpers/api-util';
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+export default function HomePage(props: any) {
   return (
     <div>
-      <EventList events={featuredEvents} />
+      <EventList events={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
