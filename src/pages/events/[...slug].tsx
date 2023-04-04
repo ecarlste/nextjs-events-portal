48211ed5e4fa7efd3fc999be33350993d52bfe57
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-03 21:00:14 -0600

    Switched from SSR to client side fetch on filtered events page

--- src/pages/events/[...slug].tsx
+++ src/pages/events/[...slug].tsx
@@ -2,50 +2,71 @@ import EventList from '@/components/events/event-list';
 import ResultsTitle from '@/components/events/results-title';
 import Button from '@/components/ui/button';
 import ErrorAlert from '@/components/ui/error-alert';
-import { getFilteredEvents } from '@/helpers/api-util';
-import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { Fragment, useEffect, useState } from 'react';
+import useSWR from 'swr';
+import Event from '@/models/event';
 
-export default function FilteredEventsPage(props: {
-  events: Event[];
-  date: Date;
-}) {
-  const { events, date } = props;
+export default function FilteredEventsPage() {
+  const [loadedEvents, setLoadedEvents] = useState<Event[]>([]);
+  const router = useRouter();
+  const filteredData = router.query.slug;
 
-  if (!events) {
+  const { data, error } = useSWR(
+    'https://nextjs-events-portal-default-rtdb.firebaseio.com/events.json',
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    console.log(`inside useEffect`);
+    if (data) {
+      const events: Event[] = [];
+
+      for (const key in data) {
+        events.push({ id: key, ...data[key] });
+      }
+
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!loadedEvents || !filteredData) {
     return <p className="center">Loading...</p>;
   }
 
-  if (events.length === 0) {
+  if (error) {
+    return <p className="center">Error loading data...</p>;
+  }
+
+  const numYear = +filteredData[0];
+  const numMonth = +filteredData[1];
+
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
+  });
+
+  if (filteredEvents.length === 0) {
     return (
       <Fragment>
         <ErrorAlert>
           <p>No events found for the specified filter...</p>
         </ErrorAlert>
-        <Button link="/">Show All Events</Button>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
       </Fragment>
     );
   }
 
   return (
     <Fragment>
-      <ResultsTitle date={date} />
-      <EventList events={events} />
+      <ResultsTitle date={new Date(numYear, numMonth - 1)} />
+      <EventList events={filteredEvents} />
     </Fragment>
   );
-}
-
-export async function getServerSideProps(context: {
-  params: { slug: string[] };
-}) {
-  const { params } = context;
-  const filteredData = params.slug;
-
-  const numYear = +filteredData[0];
-  const numMonth = +filteredData[1];
-
-  const events = await getFilteredEvents({ year: numYear, month: numMonth });
-
-  return {
-    props: { events, date: new Date(numYear, numMonth - 1) },
-  };
 }
