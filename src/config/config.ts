873c4490d9418e3e2c2da4933400a97512eb6931
Erik S. Carlsten <ecarlste@gmail.com>
2023-04-05 06:51:50 -0600

    Extracted db.connect into helper global

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+const settings = {
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASS,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+};
+
+export default settings;
--- src/pages/api/newsletter/index.ts
+++ src/pages/api/newsletter/index.ts
@@ -1,41 +1,23 @@
-import { MongoClient } from 'mongodb';
+import config from '@/config/config';
+import clientPromise from '@/helpers/mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { body, method } = req;
 
-  const dbUser = process.env.DB_USER;
-  const dbPassword = process.env.DB_PASS;
-  const dbHost = process.env.DB_HOST;
-  const dbName = process.env.DB_NAME;
-
-  console.log(dbUser, dbPassword, dbHost, dbName);
-
-  const client = new MongoClient(
-    `mongodb+srv://nextjs-events-portal:Hl7BylTF53Rre40h@cluster0.fa20kwi.mongodb.net/`
-  );
-
-  console.log('waiting for connection');
-  await client.connect();
-
-  console.log('creating db');
-  const db = client.db(dbName);
-
-  console.log('creating collection');
-  const emailsCollection = db.collection('emails');
+  const client = await clientPromise;
+  const db = client.db(config.dbName);
 
   switch (method) {
     case 'POST':
-      console.log(`email: ${body.email}`);
-      await emailsCollection.insertOne({ email: body.email });
+      console.log(`inserting email: ${body.email}`);
+      await db.collection('emails').insertOne({ email: body.email });
       res.status(201).json({ email: body.email });
       break;
     default:
       res.setHeader('Allow', ['POST']);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
-
-  client.close();
 }
 
 export default handler;
